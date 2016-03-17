285fe96f4500e4bc4ad5dc3c3405ddf14ffd3394
Martin Field <mfield@yelp.com>
2016-03-16 19:20:50 -0700

    Add a failing tests for class method with varargs only

--- tests/test_funcsigs.py
+++ tests/test_funcsigs.py
@@ -81,12 +81,16 @@ class TestFunctionSignatures(unittest.TestCase):
                 pass
             def method_with_args(self, a):
                 pass
+            def method_with_varargs(*args):
+                pass
         self.assertEqual(self.signature(Test.method),
                 (((('self', Ellipsis, Ellipsis, self_kind)),), Ellipsis))
         self.assertEqual(self.signature(Test.method_with_args), ((
                 ('self', Ellipsis, Ellipsis, self_kind),
                 ('a', Ellipsis, Ellipsis, "positional_or_keyword"),
                 ), Ellipsis))
+        self.assertEqual(self.signature(Test.method_with_varargs), ((
+                ('args', Ellipsis, Ellipsis, "var_positional"),), Ellipsis))
 
 
 if __name__ == "__main__":
